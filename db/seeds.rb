db645a797d1dc07b1553e7d61e8faaa4ed289c77
msenior526 <msenior526@gmail.com>
2021-04-16 20:01:32 -0400

    commit tree a0ad73124e7af6e4ebb19264579da2da5f0e6c52

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Harry",last_name: "Potter")
+Student.create(first_name: "Hermoine",last_name: "Granger")
+Student.create(first_name: "Ron",last_name: "Weasley")
+Student.create(first_name: "Neville",last_name: "Longbottom")
+Student.create(first_name: "Draco",last_name: "Malfoy")
+Student.create(first_name: "Sirius",last_name: "Black")
+Student.create(first_name: "Luna",last_name: "Lovegood")
